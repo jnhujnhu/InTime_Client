de05efd85a2ece4e9062b5bbb61ede6de1e3ac7e
zhangjk95 <zjk_261011@163.com>
2016-06-07 09:28:26 +0800

    Update location tracker

--- app/src/main/java/com/example/kevin/mapapplication/utils/LocationTracker.java
+++ app/src/main/java/com/example/kevin/mapapplication/utils/LocationTracker.java
@@ -10,98 +10,61 @@ import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
 
-/**
- * Created by Kevin on 2/27/16.
- */
 public class LocationTracker implements LocationListener {
 
     private Context mContext;
 
     private LocationManager mLocationManager;
 
-
-    private double Latitude;
-
-    private double Longtitude;
-
-    private float Accuracy;
+    private Location lastLocation;
 
     public LocationTracker(Context context) {
-
-        Latitude = -34;
-        Longtitude = 151;
-        Accuracy = 100;
-
         mContext = context;
         getLocation();
     }
 
     public float getCurrentAccuracy() {
-        return Accuracy;
+        return lastLocation.getAccuracy();
     }
 
     public LatLng getCurrentLatlng() {
-        return new LatLng(Latitude, Longtitude);
+        return EvilTransform.transform(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude()));
     }
 
     private void getLocation(){
         Location location;
         mLocationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
-        if(mLocationManager.getAllProviders().contains(LocationManager.GPS_PROVIDER)) {
+        if (mLocationManager.getAllProviders().contains(LocationManager.GPS_PROVIDER)) {
             try {
                 mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
                 location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                 if (location != null) {
-                    Accuracy = location.getAccuracy();
-                    Latitude = location.getLatitude();
-                    Longtitude = location.getLongitude();
-                    Log.i("Location", "From GPS");
-                } else if(mLocationManager.getAllProviders().contains(LocationManager.NETWORK_PROVIDER)) {
-                    mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, this);
-                    location = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                    if (location != null) {
-                        Accuracy = location.getAccuracy();
-                        Latitude = location.getLatitude();
-                        Longtitude = location.getLongitude();
-                        Log.i("Location", "From Network");
-                    }
-                }
-                else {
-                    Log.i("Location", "Error!");
-                    Toast.makeText(mContext, "No Location Service Available", Toast.LENGTH_LONG).show();
-                    Latitude = -1;
-                    Longtitude = -1;
+                    updateLastLocation(location);
                 }
             }catch (SecurityException se) {
                 se.printStackTrace();
             }
         }
-        else if(mLocationManager.getAllProviders().contains(LocationManager.NETWORK_PROVIDER)) {
+        if (mLocationManager.getAllProviders().contains(LocationManager.NETWORK_PROVIDER)) {
             try {
                 mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, this);
                 location = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                if(location != null) {
-                    Accuracy = location.getAccuracy();
-                    Latitude = location.getLatitude();
-                    Longtitude = location.getLongitude();
-                    Log.i("Location", "From Network");
+                if (location != null) {
+                    updateLastLocation(location);
                 }
             }catch (SecurityException se) {
                 se.printStackTrace();
             }
         }
-        else {
+        if (lastLocation == null) {
             Log.i("Location", "Error!");
             Toast.makeText(mContext, "No Location Service Available", Toast.LENGTH_LONG).show();
-            Accuracy = 0;
-            Latitude = -1;
-            Longtitude = -1;
         }
     }
 
     @Override
     public void onLocationChanged(Location location) {
-
+        updateLastLocation(location);
     }
 
     @Override
@@ -117,5 +80,68 @@ public class LocationTracker implements LocationListener {
     @Override
     public void onProviderDisabled(String provider) {
 
+    }
+
+    private void updateLastLocation(Location location) {
+        if (isBetterLocation(location, lastLocation)) {
+            lastLocation = location;
+            Log.i("Location Updated", String.format("%f,%f From %s", location.getLatitude(), location.getLongitude(), location.getProvider()));
+        }
+    }
+
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
+
+    /** Determines whether one Location reading is better than the current Location fix
+     * @param location  The new Location that you want to evaluate
+     * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+     */
+    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            // A new location is always better than no location
+            return true;
+        }
+
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            return true;
+            // If the new location is more than two minutes older, it must be worse
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                currentBestLocation.getProvider());
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Checks whether two providers are the same */
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        return provider1.equals(provider2);
     }
 }
